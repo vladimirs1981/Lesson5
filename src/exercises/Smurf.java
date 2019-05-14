eac72e16d48406b210f2c60975adaaf9a577c036
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-05-14 22:35:04 +0200

    Smurfs

--- src/exercises/Smurf.java
+++ src/exercises/Smurf.java
@@ -1,12 +1,5 @@
 package exercises;
 
-/* 
- * 1. Watch this smurf cartoon: https://www.youtube.com/watch?v=RqbpzEHuO2g
- * 2. In a Runner class, make a Handy Smurf. Use the methods below to make him eat, and print his name.
- * 
- * 5. Make a Papa Smurf and print his name, hat color and girl or boy.
- * 6. Make a Smurfette and print her name, hat color and girl or boy. */
-
 public class Smurf {
 
 	private String name;
@@ -23,19 +16,23 @@ public class Smurf {
 		System.out.println(name + " Smurf is eating Smurfberries.");
 	}
 
-	/* Papa Smurf wears a red hat, all the others are white. */
 	public String getHatColor() {
-		// 3. Fill in this method
-		return "";
+
+		if (name == "Papa") {
+			return name + " Smurf wears a red hat.";
+		} else {
+			return name + " Smurf wears a white hat.";
+		}
+
 	}
 
-	/* Smurfette is the only female Smurf. */
 	public String isGirlOrBoy() {
-		// 4. Fill in this method
-		return "";
+		if (name == "Smurfette") {
+			return name + " is a girl.";
+		} else {
+			return name + " is a boy.";
+		}
+
 	}
 
 }
-
-
-
