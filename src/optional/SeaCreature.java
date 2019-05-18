fef2ad1f90ca19d3688cf5dccf2652aa7774dec1
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-05-18 11:34:09 +0200

    SeaCreature

--- src/optional/SeaCreature.java
+++ src/optional/SeaCreature.java
@@ -1,22 +1,28 @@
 package optional;
 
-/*
- * 1. In a Runner class, make a SeaCreature called “Spongebob”. Use the methods below to make him eat, and laugh.
- * 2. Make Patrick and Squidward and print their name, have them eat, and make them laugh.
- */
-
 import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
 public class SeaCreature {
-	
-	public static void main(String[] args) {
-		SeaCreature spong = new SeaCreature("squidward");
-		spong.laugh();
-	}
 
+	public static void main(String[] args) {
+
+		SeaCreature spong1 = new SeaCreature("Spongebob");
+		spong1.eat();
+		spong1.laugh();
+
+		SeaCreature spong2 = new SeaCreature("Patrick");
+		System.out.println(spong2.getName());
+		spong2.eat();
+		spong2.laugh();
+
+		SeaCreature spong3 = new SeaCreature("Squidward");
+		System.out.println(spong3.getName());
+		spong3.eat();
+		spong3.laugh();
+	}
 
 	private String name;
 
@@ -34,8 +40,8 @@ public class SeaCreature {
 
 	public void laugh() {
 		try {
-			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(
-					"sounds/" + this.name + ".wav").toURI().toURL());
+			AudioInputStream audioInputStream = AudioSystem
+					.getAudioInputStream(new File("sounds/" + this.name + ".wav").toURI().toURL());
 			Clip clip = AudioSystem.getClip();
 			clip.open(audioInputStream);
 			clip.start();
