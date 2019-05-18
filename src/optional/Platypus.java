f4e36205953276594541eb43ded90b0331421702
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-05-18 11:42:35 +0200

    Platypus

--- src/optional/Platypus.java
+++ src/optional/Platypus.java
@@ -3,17 +3,30 @@ package optional;
 public class Platypus {
 	private String name;
 
-	void sayHi(){
-		System.out.println("The platypus " + name + " is smarter than your average platypus.");
+	public Platypus(String name) {
+		this.name = name;
 	}
 	
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	void sayHi() {
+		System.out.println("The platypus " + name + " is smarter than your average platypus.");
+	}
+
 	public static void main(String[] args) {
-		//1. Make an instance of your new pet platypus
 		
-		//2. Call the sayHi method
+		Platypus plat = new Platypus("Beak");
 		
-		//3. Create a constructor in the platypus class so that you can give your platypus a name.
+		plat.sayHi();
+		
 	}
 }
-
-
