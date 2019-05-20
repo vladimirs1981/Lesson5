37fc52bdd4ec843f56692d5fe688785ca0fff5fa
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-05-20 11:20:49 +0200

    LandOfTurtles

--- src/optional/LandOfTurtles.java
+++ src/optional/LandOfTurtles.java
@@ -1,6 +1,10 @@
 package optional;
 
+import java.util.Random;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import org.teachingextensions.logo.MultiTurtlePanel;
 import org.teachingextensions.logo.Turtle;
 
@@ -9,19 +13,33 @@ public class LandOfTurtles {
 		MultiTurtlePanel panel = new MultiTurtlePanel();
 		String galapagosIslands = "https://farm2.staticflickr.com/1104/752631367_5c5d474ba5_o.jpg";
 
-		// 1. Instantiate a JFrame object & make it visible
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 
-		// 2. Add the panel to the frame
+		frame.add(panel);
+		panel.setBackgroundImage(galapagosIslands);
 
-		// 3. Set the background image of the panel to the Galapagos Islands
+		frame.setSize(600, 400);
+		panel.setSize(600, 400);
+		Turtle leonardo = new Turtle();
+		leonardo.setX(300);
+		leonardo.setY(150);
+		panel.addTurtle(leonardo);
 
-		//4. Set the size image of the frame to 600 by 400 
+		Random r = new Random();
 
-		// 5. Instantiate a Turtle
+		for (int i = 0; i < 50; i++) {
 
-		// 6. Add the turtle to the panel 
+			Turtle toucheturtle = new Turtle();
+			toucheturtle.setX(r.nextInt(600)+300);
+			toucheturtle.setY(r.nextInt(400)+150);
+			panel.addTurtle(toucheturtle);
 
-		// 7. Put 50 Turtles on the beach
+		}
 
 	}
+
 }
+
+// 7. Put 50 Turtles on the beach
